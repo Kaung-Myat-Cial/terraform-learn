e13d97fd2b45e4d5e45004ea455ff5a23c14a169
kaungmyatcial <kaungmyatcial@gmail.com>
2021-12-21 22:24:31 +0630

    ec2 third commit

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ variable "env_prefix" {}
 variable "access_key" {}
 variable "secret_key" {}
 variable "my_ip" {}
+variable "instance_type" {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -92,6 +93,41 @@ resource "aws_default_security_group" "default-sg" {
         Name: "${var.env_prefix}-default-sg"
     }
   
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["amazon"]
+    filter {
+      name = "name"
+      values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+    }
+    filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+    }
+  
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+  
+}
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = "ec2kp"
+
+    tags = {
+        Name: "${var.env_prefix}-server"
+    }
 }
 
 /*resource "aws_security_group" "myapp-sg" {
